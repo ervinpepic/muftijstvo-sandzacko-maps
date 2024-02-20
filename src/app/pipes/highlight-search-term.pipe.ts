6d3b285bb287a38925601c18e5ff67d46203c53b
Ervin Pepic <pepic_ervin@hotmail.com>
2024-02-20 02:00:41 +0100

    merged validators and pipes into utils shared methods

--- src/app/pipes/highlight-search-term.pipe.ts
+++ src/app/pipes/highlight-search-term.pipe.ts
@@ -1,52 +1,41 @@
+// Import Angular core dependencies.
 import { Pipe, PipeTransform } from '@angular/core';
+// Import utility functions from a custom utility file.
+import * as Utils from '../utils/input-validators';
 
+/**
+ * Angular pipe to highlight search terms within a string.
+ * This pipe makes the search term stand out in the provided text by wrapping it in <strong> tags.
+ * It is marked as standalone, meaning it can be used without declaring it in an Angular module.
+ */
 @Pipe({
   name: 'highlightSearchTerm',
   standalone: true,
 })
 export class HighlightSearchTermPipe implements PipeTransform {
+
   /**
- * Highlights occurrences of the specified searchTerm within the input string by wrapping them in `<strong>` tags.
- * If the searchTerm is empty or the value is not provided, the original value is returned unmodified. This function
- * also employs character substitution logic for certain characters to handle variations in character representation,
- * making the search more flexible and inclusive of character variations.
- *
- * @param {string} value - The input string in which to highlight occurrences of the searchTerm.
- * @param {string} searchTerm - The term to search for within the input string. Case-insensitive.
- * @returns {string} The transformed string with occurrences of the searchTerm highlighted. If the searchTerm
- *                   or value is not provided, returns the original value unmodified.
- * 
- * Note: Special characters in the searchTerm are escaped to prevent issues in the regex pattern.
- */
+   * Transforms the input value by highlighting the search term.
+   * 
+   * @param {string} value - The original text in which to highlight the search term.
+   * @param {string} searchTerm - The term to highlight within the original text.
+   * @returns {string} The transformed text with the search term highlighted.
+   */
   transform(value: string, searchTerm: string): string {
+    // Return the original value immediately if no search term or value is provided.
     if (!searchTerm || !value) {
       return value;
     }
 
-    function escapeRegExp(string: string): string {
-      return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    }
-
-    function substituteChars(input: string): string {
-      const charMap: { [key: string]: string } = {
-        s: '[sš]',
-        dj: '[đdj]',
-        c: '[cćč]',
-        z: '[zž]',
-        w: 'v',
-        q: 'k',
-        x: '[ks]'
-      };
-      return Object.keys(charMap).reduce(
-        (acc, cur) => acc.replace(new RegExp(cur, 'g'), charMap[cur]),
-        input
-      );
-    }
-
-    const searchTermEscaped = escapeRegExp(searchTerm.toLowerCase());
-    const normalizedInputValue = substituteChars(searchTermEscaped);
+    // Escape any special regex characters in the search term to prevent regex errors.
+    const searchTermEscaped = Utils.escapeRegExp(searchTerm.toLowerCase());
+    // Substitute characters in the escaped search term based on predefined character mappings.
+    // This step enhances the flexibility of search term matching.
+    const normalizedInputValue = Utils.substituteChars(searchTermEscaped, Utils.searchCharMap);
+    // Create a regex pattern to match the normalized search term, ignoring case sensitivity.
     const regex = new RegExp(`(${normalizedInputValue})`, 'gi');
+    // Replace matches of the search term in the original value with the same term wrapped in <strong> tags,
+    // effectively highlighting them.
     return value.replace(regex, '<strong>$1</strong>');
   }
-
 }
--- src/app/utils/input-validators.ts
+++ src/app/utils/input-validators.ts
@@ -1,52 +1,83 @@
+// Import the unorm library for Unicode normalization.
 import unorm from 'unorm';
 
-/**
- * Normalizes a string for consistent comparison and search by applying Unicode normalization and converting to 
- * lowercase. This function specifically targets normalization for accented characters and other diacritics to ensure 
- * that strings can be matched irrespective of their original form. Additionally, it handles specific character 
- * substitutions, such as replacing 'dj' with 'đ', to accommodate certain linguistic preferences or search requirements.
- *
- * The normalization process uses the NFD (Normalization Form Decomposition) to split characters into their base 
- * characters and diacritical marks. After normalization, it removes all combining diacritical marks to leave only 
- * the base characters. This approach ensures that searches and comparisons can be performed without the influence 
- * of case sensitivity or diacritical variations.
- *
- * @param {string} inputValue - The string to be normalized.
- * @returns {string} The normalized string, with specific character substitutions applied, diacritical marks removed, 
- *                   and converted to lowercase.
- */
-const replacements: { [key: string]: string } = {
+// Object mapping specific character sequences to their normalized forms.
+const charReplacements: { [key: string]: string } = {
   'dj': 'đ',
   'q': 'k',
   'x': 'ks',
   'w': 'v',
-  // Add more replacements here
+  // Additional character replacements can be added here.
 };
-export function normalizeString(inputValue: string): string {
-  let normalizedStr = unorm.nfd(inputValue);
-  normalizedStr = normalizedStr.replace(/dj|q|x|w/g, (match: string) => replacements[match]);
-  const normalizedLowerCaseStr = normalizedStr
-    .replace(/[\u0300-\u036f]/g, '')
-    .toLowerCase();
 
-  return normalizedLowerCaseStr;
+// Object mapping characters to their search-friendly variants to improve search functionality.
+export const searchCharMap: { [key: string]: string } = {
+  's': '[sš]',
+  'dj': '[đdj]',
+  'c': '[cćč]',
+  'z': '[zž]',
+  'w': 'v',
+  'q': 'k',
+  'x': '[ks]',
+  // Additional search character mappings can be added here.
+};
+
+/**
+ * Replaces characters in the input string based on the provided character map.
+ * @param {string} input - The input string to be processed.
+ * @param {Object} charMap - The character mapping to use for replacements.
+ * @returns {string} - The processed string with characters replaced.
+ */
+export function substituteChars(input: string, charMap: { [key: string]: string }): string {
+  return Object.keys(charMap).reduce(
+    (acc, cur) => acc.replace(new RegExp(cur, 'g'), charMap[cur]),
+    input
+  );
 }
 
 /**
- * Checks if the input string is numeric.
- * @param {string} input - The input string to check.
- * @returns {boolean} True if the input is numeric, false otherwise.
+ * Escapes special characters in a string for use in a regular expression.
+ * @param {string} string - The string to be escaped.
+ * @returns {string} - The escaped string, safe for use in a regex pattern.
+ */
+export function escapeRegExp(string: string): string {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+/**
+ * Checks if the input string is strictly numeric or a numeric fraction.
+ * @param {string} input - The input string to be tested.
+ * @returns {boolean} - True if the input is numeric or a fraction, false otherwise.
  */
 export function isNumericInput(input: string): boolean {
   return /^\d+\/\d+$/.test(input) || /^\d+$/.test(input);
 }
 
 /**
- * Checks if the input value satisfy given criteria.
- * @param {string} inputField - The input string to check.
- * @returns {boolean} True if the input is numeric, false otherwise.
+ * Validates whether the input field contains only allowed characters: alphanumeric, spaces, and periods,
+ * and does not start with a space.
+ * @param {string} inputField - The input string to be validated.
+ * @returns {boolean} - True if the input is valid, false otherwise.
  */
 export function validateInputField(inputField: string): boolean {
-  const regex = /^(?=[A-Za-z0-9])([A-Za-z0-9\s.]*)(?<=[A-Za-z0-9])$/
-  return regex.test(inputField)
+  const regex = /^(?=[A-Za-z0-9])[A-Za-z0-9\s.]*$/;
+  return regex.test(inputField);
+}
+
+/**
+ * Normalizes the input string by replacing specified characters, removing diacritical marks, and converting to lowercase.
+ * @param {string} inputValue - The input string to be normalized.
+ * @returns {string} - The normalized string.
+ */
+export function normalizeString(inputValue: string): string {
+  // Generate a regex pattern based on the keys of the charReplacements object.
+  const pattern = new RegExp(Object.keys(charReplacements).join('|'), 'g');
+  // Decompose the string to separate characters from their diacritical marks.
+  let normalizedStr = unorm.nfd(inputValue);
+  // Replace specified characters using the generated regex pattern.
+  normalizedStr = normalizedStr.replace(pattern, match => charReplacements[match]);
+  // Remove diacritical marks and convert to lowercase.
+  const normalizedLowerCaseStr = normalizedStr.replace(/[\u0300-\u036f]/g, '').toLowerCase();
+
+  return normalizedLowerCaseStr;
 }
